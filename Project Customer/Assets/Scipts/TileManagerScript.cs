1c3d00e93fdaa8c362e6dcf97fdd019cd97b6178
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2023-09-11 21:20:12 +0200

    endleess road implemented

---
+++ Project Customer/Assets/Scipts/TileManagerScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tile_manager : MonoBehaviour
+{
+
+    public GameObject[] tilePrefabs;
+    public float zSpawnPos = 3;
+    public float tileLenght = 40;
+    private List<GameObject> activeTiles = new List<GameObject>();
+
+    public Transform playerTransform;
+
+    void Start()
+    {
+        //tileLenght = tilePrefabs[0].;
+
+        //first tiles
+        for (int i = 0; i < 3; i++)
+        {
+            if (i == 0)
+            {
+                SpawnTile(0);
+            }
+
+            else
+            {
+                SpawnTile(Random.Range(0, tilePrefabs.Length));
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform.position.z > zSpawnPos - tileLenght * 2)
+        {
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+            DeleteOldTile();
+        }
+    }
+
+    public void SpawnTile(int tileIndex)
+    {
+        GameObject newTile = Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawnPos, transform.rotation);
+        activeTiles.Add(newTile);
+        zSpawnPos += tileLenght;
+    }
+
+    private void DeleteOldTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+}

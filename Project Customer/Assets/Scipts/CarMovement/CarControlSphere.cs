49a4304c065c558456482b79489145c92fb5b7f9
Vlad <big_vlad2002@yahoo.com>
2023-09-12 13:48:20 +0200

    Finished basic car controls
    Scripted acceleration, turning, car falling, car rotation, wheel turning

--- Project Customer/Assets/Scipts/CarMovement/CarControlSphere.cs
+++ Project Customer/Assets/Scipts/CarMovement/CarControlSphere.cs
@@ -5,9 +5,19 @@ public class CarControlSphere : MonoBehaviour
 
     public Rigidbody spehere;
 
-    public float forwardAcc = 3f, backAcc = 2f, maxSpeed = 50f, turnStrength = 180f;
+    public float forwardAcc = 3f, backAcc = 2f, maxSpeed = 50f, turnStrength = 180f, gravityForce= 10f, dragOnGround = 3f;
 
     private float speedInput, turnInput;
+
+    public bool grounded;
+
+    public LayerMask whatIsGround;
+
+    public float groundRayLength = 0.5f;
+    public Transform groundRayPoint;
+
+    public Transform leftFrontWheel, rightFrontWheel;
+    public float maxWheelTurn = 25f;
 
     // Start is called before the first frame update
     void Start()
@@ -33,13 +43,37 @@ public class CarControlSphere : MonoBehaviour
         turnInput = Input.GetAxis("Horizontal");
 
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
+
+        leftFrontWheel.localRotation = Quaternion.Euler(leftFrontWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn) - 180, leftFrontWheel.localRotation.eulerAngles.z);
+        rightFrontWheel.localRotation = Quaternion.Euler(rightFrontWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn), rightFrontWheel.localRotation.eulerAngles.z);
+
     }
 
     void FixedUpdate()
     {
-        if (Mathf.Abs(speedInput) > 0)
+        grounded = false;
+        RaycastHit hit;
+
+        if(Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLength,whatIsGround))
         {
-            spehere.AddForce(transform.forward * speedInput);
+            grounded = true;
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
         }
+
+        if (grounded)
+        {
+            spehere.drag = dragOnGround;
+            if (Mathf.Abs(speedInput) > 0)
+            {
+                spehere.AddForce(transform.forward * speedInput);
+            }
+        }
+        else
+        {
+            spehere.drag = 0.1f;
+            spehere.AddForce(Vector3.up * -gravityForce * 100);
+        }
+
+        
     }
 }

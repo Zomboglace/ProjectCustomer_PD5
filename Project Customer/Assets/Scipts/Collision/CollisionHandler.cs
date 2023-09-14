50b8c27531f3f5f4a36347b512a6f43170a8e633
Vlad <big_vlad2002@yahoo.com>
2023-09-14 15:39:31 +0200

    Made basic collision mechanics

---
+++ Project Customer/Assets/Scipts/Collision/CollisionHandler.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    public AutoAccelerate carMovement;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            carMovement.enabled = false;
+            Debug.Log("GAMEOVER!!!");
+        }
+    }
+}

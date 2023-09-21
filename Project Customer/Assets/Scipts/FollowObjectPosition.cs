5a32fd3a4ec41695562d038dea1e7fb4ccd1a28b
Nicolas <63503648+Zomboglace@users.noreply.github.com>
2023-09-21 15:12:14 +0200

    Revert "New drunk"

--- Project Customer/Assets/Scipts/FollowObjectPosition.cs
+++ Project Customer/Assets/Scipts/FollowObjectPosition.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FollowObjectPosition : MonoBehaviour
 {
     public GameObject objectToFollow;
+    public float smoothSpeed = 1f; // Smoothing factor for camera movement
 
     // Start is called before the first frame update
     void Start()
@@ -12,9 +13,17 @@ public class FollowObjectPosition : MonoBehaviour
         
     }
 
-    void LateUpdate()
+    void Update()
     {
         // Only follow the z position of the object
-        transform.position = new Vector3(transform.position.x, transform.position.y, objectToFollow.transform.position.z);
+        Vector3 desiredPosition = new Vector3(transform.position.x, transform.position.y, objectToFollow.transform.position.z);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
     }
+
+    // Update is called once per frame
+  /* void FixedUpdate()
+    {
+       
+    }*/
 }

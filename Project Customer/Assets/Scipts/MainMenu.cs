5de9592ffacd9df239afeace4bb2a102b00e0a69
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2023-09-21 12:21:17 +0200

    main menu working, no text in it

--- Project Customer/Assets/Scipts/MainMenu.cs
+++ Project Customer/Assets/Scipts/MainMenu.cs
@@ -5,8 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public GameObject rules;
+    public GameObject options;
+
+    void Start()
+    {
+        rules.SetActive(false);
+        options.SetActive(false);
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+
+    public void ShowRules()
+    {
+        rules.SetActive(true);
+    }
+
+    public void ShowSettings()
+    {
+        options.SetActive(true);
+    }
+
+    public void GoBackRules()
+    {
+        rules.SetActive(false);
+        options.SetActive(false);
     }
 }

f13fc8f4700368aedf471ac0195d7c1d116c1b78
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2023-09-22 10:33:00 +0200

    menu shit complete hopefully

--- Project Customer/Assets/Scipts/MainMenu.cs
+++ Project Customer/Assets/Scipts/MainMenu.cs
@@ -6,13 +6,18 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
+    public AudioSource src;
+    public AudioClip sfx1;
+
     public GameObject gameTittle;
-    public GameObject rules;
+    public GameObject rules, rules2;
+    public GameObject nextPanelButton;
     public GameObject options;
 
     void Start()
     {
         rules.SetActive(false);
+        rules2.SetActive(false);
         options.SetActive(false);
     }
 
@@ -25,6 +30,13 @@ public class MainMenu : MonoBehaviour
     {
         rules.SetActive(true);
         gameTittle.SetActive(false);
+        nextPanelButton.SetActive(true);
+    }
+
+    public void ShowNextRules()
+    {
+        rules2.SetActive(true);
+        nextPanelButton.SetActive(false);
     }
 
     public void ShowSettings()
@@ -36,7 +48,15 @@ public class MainMenu : MonoBehaviour
     public void GoBackRules()
     {
         gameTittle.SetActive(true);
+        rules2.SetActive(false);
         rules.SetActive(false);
         options.SetActive(false);
     }
+
+    public void Button1()
+    {
+        src.clip = sfx1;
+        src.Play();
+    }
+
 }
